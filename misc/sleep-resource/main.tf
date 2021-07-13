5586696fa4d1c2f149759f39c3d9ff076f381761
Eyal Atzmon <eyal.atzmon@env0.com>
2021-07-13 12:43:45 +0300

    increase sleep to 120 sec

--- misc/sleep-resource/main.tf
+++ misc/sleep-resource/main.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait_120_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "90s"
+  create_duration = "120s"
 }
 
-# This resource will create (at least) 30 seconds after null_resource.previous
+# This resource will create (at least) 120 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
+  depends_on = [time_sleep.wait_120_seconds]
 }
